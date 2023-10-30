587171b7e61f76cdc5b67924678d5dc4777d3a22
Frances Morales Velilla <frances0688@gmail.com>
2023-10-30 21:21:49 +0100

    Create endpoint to add new users

---
+++ controllers/UserController.js
@@ -0,0 +1,13 @@
+const {User} = require("../models/index.js");
+
+const UserController = {
+    create(req, res) {
+        req.body.role = "user";
+        User
+            .create(req.body)
+            .then((user) => res.status(201).send({message: "User created", user}))
+            .catch((err) => console.error(err));
+    }
+};
+
+module.exports = UserController;
