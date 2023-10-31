507c21d4b31f27ba1d6f807629dfab8468228b17
Frances Morales Velilla <frances0688@gmail.com>
2023-10-31 10:21:09 +0100

    Use bcrypt for passwords and create endpoint to login user

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,13 +1,49 @@
-const {User} = require("../models/index.js");
+const { User } = require("../models/index.js");
+const bcrypt = require("bcryptjs");
 
 const UserController = {
-    create(req, res) {
-        req.body.role = "user";
-        User
-            .create(req.body)
-            .then((user) => res.status(201).send({message: "User created", user}))
-            .catch((err) => console.error(err));
-    }
+	create(req, res) {
+		req.body.role = "user";
+		const password = bcrypt.hashSync(req.body.password, 10);
+		User.create({
+			...req.body,
+			password,
+		})
+			.then((user) =>
+				res
+					.status(201)
+					.send({ message: "User created", user })
+			)
+			.catch((err) => console.error(err));
+	},
+
+	login(req, res) {
+		User.findOne({
+			where: {
+				email: req.body.email,
+			},
+		}).then((user) => {
+			if (!user) {
+				return res
+					.status(400)
+					.send({
+						message: "Incorrect username or password",
+					});
+			}
+			const isMatch = bcrypt.compareSync(
+				req.body.password,
+				user.password
+			);
+			if (!isMatch) {
+				return res
+					.status(400)
+					.send({
+						message: "Incorrect username or password",
+					});
+			}
+			res.send(user);
+		});
+	},
 };
 
 module.exports = UserController;
