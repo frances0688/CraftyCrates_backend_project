c87660c2bf27a6d878fa6bf47ca7f088955592aa
Frances Morales Velilla <frances0688@gmail.com>
2023-11-23 22:38:16 +0100

    Add cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
-const { typeError } = require("./middleware/errors.js");
+const {typeError} = require("./middleware/errors.js");
+const cors = require("cors");
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/users", require("./routes/users"));
@@ -10,7 +12,7 @@ app.use("/themes", require("./routes/themes"));
 app.use("/boxes", require("./routes/boxes"));
 app.use("/products", require("./routes/products"));
 app.use("/orders", require("./routes/orders"));
-app.use("/reviews", require("./routes/reviews"));
+// app.use("/reviews", require("./routes/reviews"));
 
 app.use(typeError);
 
